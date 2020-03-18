863235ab69c70cb2d578daa235d0370eac4e98d8
nairanjali0508 <nairanjali0508@gmail.com>
2020-03-18 23:28:10 +0530

    Login and Register Page

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:bloggingapp/LoginRegisterPage.dart';
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: "Blogging App",
+      theme: new ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: LoginRegisterPage(),
+    );
+  }
+}
