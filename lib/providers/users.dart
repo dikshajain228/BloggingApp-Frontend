99a9b8d45c3539ffc8f635aad3f26eabd51117d7
battulapragati <53899365+battulapragati@users.noreply.github.com>
2020-03-27 19:04:22 +0530

    updated users provider

--- lib/providers/users.dart
+++ lib/providers/users.dart
@@ -11,16 +11,101 @@ class Users with ChangeNotifier {
       is_following: false,
       profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
     ),
+
+  new User(
+       user_id: 2,
+      email: "diksha7354@gmail.com",
+      username: "diksha",
+      about: "Lets's catch up to know more about me.",
+      profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
+    ),
+    new User(
+       user_id: 3,
+      email: "ananyasheshu@gmail.com",
+      username: "ananya",
+      about: "Sky is the limit.",
+      profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
+    ),
+    new User(
+       user_id: 4,
+      email: "pragatibattula@gmail.com",
+      username: "pragati",
+      about: "Whatever will be,will be",
+      profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
+    ),
   ];
   List<User> get users{
     return [..._users];
   }
-  User getUserProfile(){
-    return User(user_id: 1,
+
+
+ /**Future<void> getUserProfile()async {
+   List<User> fetchedData = [];
+    fetchedData.add(User(user_id: 1,
         is_following: false,
         email: "nairanjali0508@gmail.com",
         username: "anjalay",
         about: "HElo gurls nd bois ssup with y'all. How is corona treating you? Hope all are infected",
-        profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk");
+        profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"));
+
+    fetchedData.add(User(
+      user_id: 2,
+      is_following: false,
+      email: "diksha7354@gmail.com",
+      username: "diksha",
+      about: "Lets's catch up to know more about me.",
+      profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
+    ));
+    fetchedData.add(User(
+      user_id: 3,
+      is_following: false,
+      email: "ananyasheshu@gmail.com",
+      username: "ananya",
+      about: "Sky is the limit.",
+      profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
+    ));
+     fetchedData.add(User(
+      user_id: 4,
+      is_following: false,
+      email: "pragatibattula@gmail.com",
+      username: "pragati",
+      about: "Whatever will be,will be",
+      profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
+     ));
+  }**/
+
+   User getUserProfile(){
+    return User(
+        user_id: 1,
+        is_following: false,
+        email: "nairanjali0508@gmail.com",
+        username: "anjalay",
+        about: "HElo gurls nd bois ssup with y'all. How is corona treating you? Hope all are infected",
+        profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
+      );
+     return User(
+      user_id: 2,
+      is_following: false,
+      email: "diksha7354@gmail.com",
+      username: "diksha",
+      about: "Lets's catch up to know more about me.",
+      profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
+    );
+    return User(
+      user_id: 3,
+      is_following: false,
+      email: "ananyasheshu@gmail.com",
+      username: "ananya",
+      about: "Sky is the limit.",
+      profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
+    );
+     return User(
+      user_id: 4,
+      is_following: false,
+      email: "pragatibattula@gmail.com",
+      username: "pragati",
+      about: "Whatever will be,will be",
+      profile_image_url: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk"
+     );
   }
 }
