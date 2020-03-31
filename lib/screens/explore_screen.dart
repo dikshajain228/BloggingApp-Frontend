d99f0bba8ebc2d1f81781906f7da26e251a11514
Ananya <ananyasheshu@gmail.com>
2020-03-31 13:32:56 +0530

    Added search screen

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -6,8 +6,8 @@ import '../providers/articles.dart';
 
 import '../widgets/drawer.dart';
 import '../widgets/collection_list.dart';
-import '../widgets/user_list.dart';
 import '../widgets/articles_list.dart';
+import '../widgets/user_list.dart';
 
 class ExploreScreen extends StatefulWidget {
   static const routeName = "/explore";
@@ -15,11 +15,18 @@ class ExploreScreen extends StatefulWidget {
   _ExploreScreenState createState() => _ExploreScreenState();
 }
 
+enum SearchStatus { NoQuery, Searching, Searched }
+
 class _ExploreScreenState extends State<ExploreScreen>
     with SingleTickerProviderStateMixin {
   var _isInit = true;
   var _isLoading = false;
   TabController tabController;
+  TextEditingController searchController;
+
+  String searchText;
+
+  SearchStatus searchStatus;
 
   @override
   void initState() {
@@ -29,13 +36,13 @@ class _ExploreScreenState extends State<ExploreScreen>
       length: 3,
       initialIndex: 0,
     );
-    print("Collection");
+    searchController = TextEditingController();
+    searchText = "";
+    searchStatus = SearchStatus.NoQuery;
   }
 
   @override
   void didChangeDependencies() {
-    Provider.of<Collections>(context).getCollections();
-    Provider.of<Articles>(context).getArticles();
     super.didChangeDependencies();
   }
 
@@ -45,24 +52,88 @@ class _ExploreScreenState extends State<ExploreScreen>
     super.dispose();
   }
 
+  void search() {
+    Provider.of<Collections>(context).getCollections();
+    Provider.of<Articles>(context).getArticles();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text("Explore"),
-            bottom: TabBar(controller: tabController, tabs: [
-              Tab(text: "Collections"),
-              Tab(text: "Articles"),
-              Tab(text: "People"),
-            ])),
-        body: TabBarView(
-          controller: tabController,
-          children: <Widget>[
-            CollectionList(),
-            ArticlesList(),
-            UserList(),
+          title: TextField(
+            autofocus: false,
+            controller: searchController,
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.teal[400],
+              hintText: "Search...",
+              hintStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+              enabledBorder: InputBorder.none,
+              border: InputBorder.none,
+            ),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+            textInputAction: TextInputAction.search,
+            onSubmitted: (text) {
+              // send a query
+              setState(() {
+                searchText = searchController.text;
+                searchStatus = SearchStatus.Searching;
+              });
+              print(text);
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.cancel,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                searchController.text = "";
+                setState(() {
+                  searchText = "";
+                });
+              },
+            )
           ],
+          bottom: (searchText == ""
+              ? null
+              : TabBar(controller: tabController, tabs: [
+                  Tab(text: "Collections"),
+                  Tab(text: "Articles"),
+                  Tab(text: "People"),
+                ])),
         ),
+        body: (searchStatus == SearchStatus.NoQuery
+            ? Center(
+                child: Container(
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image:
+                          ExactAssetImage("assests/images/person_search.png"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              )
+            : TabBarView(
+                controller: tabController,
+                children: <Widget>[
+                  CollectionList(),
+                  ArticlesList(),
+                  UserList(),
+                ],
+              )),
         drawer: MainDrawer());
   }
 }
