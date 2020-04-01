e8238da81a3a93f75dc8791aae6f23b8613bb127
DikshaGiria <57097322+DikshaGiria@users.noreply.github.com>
2020-04-01 12:53:35 +0530

    article delete screen added

---
+++ lib/screens/article_delete_screen.dart
@@ -0,0 +1,28 @@
+import 'package:bloggingapp/widgets/article_delete_list.dart';
+import 'package:flutter/material.dart';
+import '../widgets/article_delete_list.dart';
+import '../widgets/drawer.dart';
+
+class ArticleDeleteScreen extends StatefulWidget {
+  static const routeName = "/delete-article-page";
+
+  @override
+  _ArticleDeleteScreenState createState() => _ArticleDeleteScreenState();
+}
+
+class _ArticleDeleteScreenState extends State<ArticleDeleteScreen> {
+  void initState() {
+    print("HEllo I am in delete article init state");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Delete Article'),
+      ),
+      body: ArticlesDeleteList(),
+      drawer: MainDrawer(),
+    );
+  }
+}
