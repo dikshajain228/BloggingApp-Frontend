bda516e17065f86e85b4756d78204dfddff98dec
battulapragati <53899365+battulapragati@users.noreply.github.com>
2020-04-01 12:35:29 +0530

    updated drawer.

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -12,6 +12,7 @@ import '../screens/profile_page.dart';
 import '../screens/article_insert_screen.dart';
 import '../screens/article_edit_screen.dart';
 
+
 class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,21 +20,13 @@ class MainDrawer extends StatelessWidget {
       child: new ListView(
         children: <Widget>[
           new UserAccountsDrawerHeader(
-            accountName:
-                new Text("someusername"), //has to come  from login screen.
-            currentAccountPicture: new GestureDetector(
-              child: new CircleAvatar(
-                backgroundImage: new NetworkImage(
-                    "https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
-              ),
-              onTap: () => print("This is the current user."),
-            ),
+           
             decoration: new BoxDecoration(
                 image: new DecorationImage(
                     image: new NetworkImage(
                         "https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
                     fit: BoxFit.fill)),
-            accountEmail: null,
+            accountEmail: null,accountName:null,
           ),
           new ListTile(
               leading: Icon(Icons.person),
@@ -55,9 +48,9 @@ class MainDrawer extends StatelessWidget {
               }),
           new ListTile(
               leading: Icon(Icons.collections),
-              title: new Text("Collections"),
+              title: new Text(" Add Collections"),
               onTap: () {
-                Navigator.of(context).pushNamed(ProfilePage.routeName);
+                Navigator.of(context).pushNamed(CollectionInsertScreen.routeName);
               }),
           new ListTile(
               leading: Icon(Icons.explore),
@@ -65,30 +58,8 @@ class MainDrawer extends StatelessWidget {
               onTap: () {
                 Navigator.of(context).pushNamed(ExploreScreen.routeName);
               }),
-          new ListTile(
-              leading: Icon(Icons.library_books),
-              title: new Text("Your Articles"),
-              onTap: () {
-                Navigator.of(context).pushNamed(YourArticles.routeName);
-              }),
-          ListTile(
-            title: Text("New article - delete"),
-            onTap: () {
-              Navigator.of(context).pushNamed(ArticleInsertScreen.routeName);
-            },
-          ),
-          ListTile(
-            title: Text("Edit article"),
-            onTap: () {
-              Navigator.of(context).pushNamed(ArticleEditScreen.routeName);
-            },
-          ),
-          ListTile(
-            title: Text("Add collection"),
-            onTap: () {
-              Navigator.of(context).pushNamed(CollectionInsertScreen.routeName);
-            },
-          ),
+         
+          
           new Divider(),
           new ListTile(
               title: new Text("Logout"),
