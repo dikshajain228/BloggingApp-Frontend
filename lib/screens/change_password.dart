15d1054e69ae88ac54795857265a800d5ecbe6d4
battulapragati <53899365+battulapragati@users.noreply.github.com>
2020-04-08 23:35:38 +0530

    added toast message.

--- lib/screens/change_password.dart
+++ lib/screens/change_password.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:toast/toast.dart';
 
 import '../screens/profile_page.dart';
 
@@ -96,27 +97,22 @@ class ChangePasswordState extends State<ChangePassword> {
                       String oldPassword = _oldPasswordController.text;
                       String newPassword = _newPassword1Controller.text;
                           status = await Provider.of<User>(context).changePassword(oldPassword,newPassword);
-                          //Change this to snackbars
+                          
                           print(status);
                       if (status == 401)
-                        displayDialog(context,
-                           "Invalid password",
-                         "Check your current password",
-                        );
+                          Toast.show("Invalid password.Check your current password.", context, duration:7, gravity:  Toast.BOTTOM);
+                        
                       else if (status == 200){
-                        //snackbar
                         print("Password changed successfully");
-                        // displayDialog(
-                        //   context,"Success",
-                        //   "Changed password successfully",
-                        // );
                         Navigator.of(context).pushReplacementNamed(ProfilePage.routeName);
+                        Toast.show("Password changed successfully.", context, duration:7, gravity:  Toast.BOTTOM);
                       }
                       else
                         displayDialog(
                           context,"Error",
                           "Unknown error occurred",
                         );
+                       // Toast.show("Unknown error occurred.Please try agin.", context, duration:7, gravity:  Toast.BOTTOM);
                     }
                    
                   },
