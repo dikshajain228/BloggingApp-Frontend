59958ba71e99e99e7e6045bcd432b46b34551bdf
Ananya <ananyasheshu@gmail.com>
2020-04-09 18:30:11 +0530

    Refactored code

--- lib/providers/users.dart
+++ lib/providers/users.dart
@@ -113,18 +113,6 @@ class Users with ChangeNotifier {
     }
   }
 
-  User getUserProfile() {
-    return User(
-        user_id: 1,
-        is_following: false,
-        email: "nairanjali0508@gmail.com",
-        username: "anjalay",
-        about:
-            "HElo gurls nd bois ssup with y'all. How is corona treating you? Hope all are infected",
-        profile_image_url:
-            "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk");
-  }
-
   Future<void> searchUsers(String query) async {
     List<User> fetchedUsers = [];
     final token = await storage.read(key: "token");
