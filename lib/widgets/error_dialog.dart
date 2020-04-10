64310ae9b34df25f4fcb896061f81c444de7ec1f
Ananya <ananyasheshu@gmail.com>
2020-04-10 17:42:56 +0530

    Changed styling

--- lib/widgets/error_dialog.dart
+++ lib/widgets/error_dialog.dart
@@ -6,11 +6,31 @@ class ErrorDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text("Error..."),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))),
+      titlePadding: EdgeInsets.all(0),
+      title: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Text(
+          'Error...',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
       content: Text(errorMessage),
       actions: <Widget>[
         FlatButton(
           child: Text("OK"),
+          textColor: Colors.black,
+          splashColor: Colors.black,
           onPressed: () {
             Navigator.of(context).pop();
           },
