68279ad531091f3f1bb95ad11bc696d53421b3b7
dikshajain228 <52117907+dikshajain228@users.noreply.github.com>
2020-04-14 18:11:46 +0530

    Update opinions.dart

--- lib/providers/opinions.dart
+++ lib/providers/opinions.dart
@@ -1,34 +1,155 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+
+import '../server_util.dart' as Server;
 import './opinion.dart';
 
 class Opinions with ChangeNotifier{
-  List<Opinion> _opinions = [
-    new Opinion(
-      opinion_id: "1",
-      article_id: "1",
-      username: "anjali",
-      content: "Wonderful article so proud of you my babies there us so much in this workd for you to see i dont know why am i doing this but i just want to see if it overflows",
-      opinion_date: DateTime.parse("1969-07-20 20:18:04Z"),
-      user_profile_image_path: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk",
+  static const baseUrl = Server.SERVER_IP + "/api/v1/";
+  final storage = FlutterSecureStorage();
+  List<Opinion> _opinions = [];
+  List<Opinion> _replies = [];
 
-    ),
-    new Opinion(
-      opinion_id: "1",
-      article_id: "1",
-      username: "anjali",
-      content: "Wonderful article",
-      opinion_date: DateTime.parse("1969-07-20 20:18:04Z"),
-      user_profile_image_path: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk",
-    ),
-    new Opinion(
-      opinion_id: "1",
-      article_id: "1",
-      username: "nitesh",
-      content: "You suck yo why you even doimg this",
-      opinion_date: DateTime.parse("2020-07-20 20:18:04Z"),
-      user_profile_image_path: "https://media-exp1.licdn.com/dms/image/C5103AQGDxa-dvHX5eQ/profile-displayphoto-shrink_800_800/0?e=1590624000&v=beta&t=vWGIOS-ZkaM6DzyWhgs70OipVbqTfFfKoFQPndpIkvk",
-    )
-  ];
+  // Get Opinions of an article
+  Future<void> getOpinions(String articleId) async {
+    List<Opinion> fetchedOpinions = [];
+    final token = await storage.read(key: "token");
+    String url = baseUrl + "articles/" + articleId + "/opinions";
+    try {
+      final response = await http.get(
+        url,
+        headers: {HttpHeaders.authorizationHeader: token},
+      );
+      if (response.statusCode == 200) {
+        final responseJson = json.decode(response.body);
+        for (final opinion in responseJson["opinions"]){
+          fetchedOpinions.add(Opinion(
+            opinion_id: opinion["opinion_id"],
+            article_id: opinion["article_id"],
+            username: opinion["username"],
+            user_profile_image_path: opinion["profile_image_url"],
+            content: opinion["content"],
+            opinion_date: opinion["date_created"],
+         ));
+        }
+        _opinions = [...fetchedOpinions];
+        notifyListeners();
+      } else if (response.statusCode == 404) {
+        print("Opinions not found");
+        throw "Opinions not found";
+      } else {
+        print(response.body);
+        throw "Failed to load Opinions";
+      }
+    } catch (error) {
+      print(error);
+      throw "Failed to load opinions";
+    }
+  }
+
+  // Add new Opinion
+  Future<String> addOpinion(Map<String, dynamic> data) async {
+    print(data);
+    final token = await storage.read(key: "token");
+    final userId = await storage.read(key: "userId");
+    final articleId = await storage.read(key: "articleId");
+
+    String url = baseUrl +  "articles/" + articleId + "opinions";
+    DateFormat dateFormatter = new DateFormat('yyyy-MM-dd');
+    String dateCreated = dateFormatter.format(DateTime.now());
+    String isReply= (data["is_reply"]=="1"?"1":"0");
+    var request = http.MultipartRequest('POST', Uri.parse(url));
+
+    request.headers["Authorization"] = token;
+    request.fields["user_id"] = userId;
+    request.fields["article_id"] = articleId;
+    request.fields["content"] = data["content"];
+    request.fields["date_created"] = dateCreated;
+    request.fields["is_reply"]=isReply;
+    try {
+      dynamic response = await request.send();
+      response = await response.stream.bytesToString();
+      final responseJson = json.decode(response);
+      if (responseJson["error"] == false) {
+        return "Successfully inserted opinion";
+      } else {
+        print(responseJson["message"]);
+        throw "Failed to insert opinion";
+      }
+    } catch (error) {
+      throw "Failed to insert opinion";
+    }
+  }
+
+  // Get All replies of an Opinion
+  Future<void> getAllReplies(String articleId, String opinionId) async {
+    List<Opinion> fetchedReplies = [];
+    String url = baseUrl +  "articles/" + articleId + "/opinions" + opinionId;
+    final token = await storage.read(key: "token");
+    await http.get(
+      url,
+      headers: {HttpHeaders.authorizationHeader: token},
+    ).then((response) {
+      if (response.statusCode == 200) {
+        final responseJson = json.decode(response.body);
+        for (final opinion in responseJson["opinions"]) {
+          fetchedReplies.add(Opinion(
+            opinion_id: opinion["opinion_id"],
+            article_id: opinion["article_id"],
+            username: opinion["username"],
+            user_profile_image_path: opinion["profile_image_url"],
+            content: opinion["content"],
+            opinion_date: opinion["date_created"],
+          ));
+        }
+        _replies = [...fetchedReplies];
+        notifyListeners();
+        //return _replies;
+      } else {
+        print(response.body);
+        throw "Failed to load replies";
+      }
+    }).catchError((error) {
+      print(error);
+      throw "Failed to load replies";
+    });
+  }
+
+
+  // List<Opinion> _opinions = [
+  //   new Opinion(
+  //     opinion_id: "1",
+  //     article_id: "1",
+  //     username: "anjali",
+  //     content: "Wonderful article so proud of you my babies there us so much in this workd for you to see i dont know why am i doing this but i just want to see if it overflows",
+  //     opinion_date: DateTime.parse("1969-07-20 20:18:04Z"),
+  //     user_profile_image_path: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk",
+
+  //   ),
+  //   new Opinion(
+  //     opinion_id: "1",
+  //     article_id: "1",
+  //     username: "anjali",
+  //     content: "Wonderful article",
+  //     opinion_date: DateTime.parse("1969-07-20 20:18:04Z"),
+  //     user_profile_image_path: "https://media-exp1.licdn.com/dms/image/C5103AQHBDtEzuau2rA/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=ZhtIT5ULua7ZmYzouKF2j4wHzTbFLdbxMcVTRjDHKFk",
+  //   ),
+  //   new Opinion(
+  //     opinion_id: "1",
+  //     article_id: "1",
+  //     username: "nitesh",
+  //     content: "You suck yo why you even doimg this",
+  //     opinion_date: DateTime.parse("2020-07-20 20:18:04Z"),
+  //     user_profile_image_path: "https://media-exp1.licdn.com/dms/image/C5103AQGDxa-dvHX5eQ/profile-displayphoto-shrink_800_800/0?e=1590624000&v=beta&t=vWGIOS-ZkaM6DzyWhgs70OipVbqTfFfKoFQPndpIkvk",
+  //   )
+  // ];
   List<Opinion> get opinions{
     return [..._opinions];
   }
